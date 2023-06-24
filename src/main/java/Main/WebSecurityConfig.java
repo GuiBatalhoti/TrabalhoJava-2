2b75c077cd655f3cd6c6fca9a150fabb4c471b3e
GuiBatalhoti <guibatalhoti@gmail.com>
2023-06-24 14:31:37 -0300

    removing unused imports

--- src/main/java/Main/WebSecurityConfig.java
+++ src/main/java/Main/WebSecurityConfig.java
@@ -1,13 +1,7 @@
 package Main;
 
-import Main.Model.Users;
-import Main.Repository.UsuarioRepository;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -15,9 +9,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Configuration
 @EnableMethodSecurity
